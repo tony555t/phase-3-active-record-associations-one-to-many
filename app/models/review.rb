7fefb57b0e351acd0692ba7472b5ac639e096ea7
tony555t <tonytullah764@gmail.com>
2023-02-28 11:27:30 +0300

    one to many

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
-class Review < ActiveRecord::Base
+# class Review < ActiveRecord::Base
+#     # a review belongs to a game
+#     def game
+#       # self is the review instance
+#       Game.find(self.game_id)
+#     end
   
-end
+#   end
+class Review < ActiveRecord::Base
+    belongs_to :game
+  end
+  
